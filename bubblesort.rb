1a4cae401f36709928a23ff7d6e63fbd09b977b0
Alex Withington-Smith <alex@awithsmith.com>
2020-10-03 18:04:34 +0100

    now working with any number in array - removed unneeded case for 2

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,15 +1,24 @@
+# frozen_string_literal: true
+
 # array -> array
 # returns sorted array
 def bubble_sort(array)
   # base case - empty or array of 1
   # return original array
   return array if array.length < 2
-  
-  # next most simple case: 2 
-  if array.length == 2
-    array[1] > array[0] ? array : [array[1], array[0]]
+
+  # all other cases
+  (array.length - 1).times do
+    (0..array.length - 2).map do |index|
+      next unless array[index] > array[index + 1]
+
+      temp = array[index + 1]
+      array[index + 1] = array[index]
+      array[index] = temp
+    end
   end
+  array
 end
 
-unsorted = [8, 3]
+unsorted = [8, 3, 6, 9, 1, 6, 9, 69, 596, 12, 1234]
 p bubble_sort(unsorted)
