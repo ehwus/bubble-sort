e9ad0ee736664e82e86724598fe218bbe78e9bb9
Alex Withington-Smith <alex@awithsmith.com>
2020-10-03 16:23:45 +0100

    algorithm now works with 2 array values

--- bubblesort.rb
+++ bubblesort.rb
@@ -4,8 +4,12 @@ def bubble_sort(array)
   # base case - empty or array of 1
   # return original array
   return array if array.length < 2
-
+  
+  # next most simple case: 2 
+  if array.length == 2
+    array[1] > array[0] ? array : [array[1], array[0]]
+  end
 end
 
-unsorted = [1]
-puts bubble_sort(unsorted)
+unsorted = [8, 3]
+p bubble_sort(unsorted)
